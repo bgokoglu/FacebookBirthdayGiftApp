ac58df0133ae13cdaabd6d0470406a34fee9d5d3
bgokoglu <burak.gokoglu@yahoo.com>
2014-04-01 16:08:46 -0500

    Initial Commit

---
+++ FacebookBirthdayGiftApp/Helpers/RecommendationEngine.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FacebookBirthdayGiftApp.Models;
+
+namespace FacebookBirthdayGiftApp.Helpers
+{
+    public static class RecommendationEngine
+    {
+        private static readonly string[] CategoriesToConsider =
+        {
+            "Entertainment",
+            "Tv show",
+            "Movie",
+            "Book",
+            "Musician/band",
+            "Product/service",
+            "Clothing",
+            "Food/beverages",
+            "Shopping/retail",
+            "Games/toys"
+        };
+
+        public static async Task<List<RecommendedItem>> RecommendProductAsync(MyAppUserFriend friend)
+        {
+            var recommendedItem = new List<RecommendedItem>();
+            if (friend.Likes != null)
+            {
+                var friendLikes = friend.Likes.Data;
+                var friendLikesFilteredByCategory = friendLikes.Where(like => CategoriesToConsider.Contains(like.Category));
+
+                foreach (var item in friendLikesFilteredByCategory.Take(10))
+                {
+                    Semantics3ShoppingSearchClient.GetProductsAsync(item.Name);
+                    var result = await ShoppingSearchClient.GetProductsAsync(item.Name);
+                    if (result.items == null)
+                    {
+                        // skip empty results
+                        continue;
+                    }
+                    var products = result.items.Select(i => i.product).Take(2).Where(p => p != null);
+                    foreach (var product in products)
+                    {
+                        recommendedItem.Add(new RecommendedItem
+                        {
+                            Product = product,
+                            Reason = "likes " + item.Name
+                        });
+                    }
+                }
+            }
+
+            return recommendedItem;
+        }
+    }
+}
---
+++ FacebookBirthdayGiftApp/Helpers/ShoppingSearchClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FacebookBirthdayGiftApp.Models;
+using Newtonsoft.Json.Linq;
+using Semantics3;
+
+namespace FacebookBirthdayGiftApp.Helpers
+{
+    public static class ShoppingSearchClient
+    {
+        private const string SearchApiTemplate = "https://www.googleapis.com/shopping/search/v1/public/products?key={0}&country=US&q={1}&alt=json";
+        private static readonly HttpClient client = new HttpClient();
+
+        public static string AppKey = ConfigurationManager.AppSettings["Search:AppKey"];
+
+        public static Task<SearchResult> GetProductsAsync(string query)
+        {
+            if (String.IsNullOrEmpty(AppKey))
+            {
+                throw new InvalidOperationException("Search:AppKey cannot be empty. Make sure you set it in the configuration file.");
+            }
+
+            query = query.Replace(" ", "+");
+            string searchQuery = String.Format(SearchApiTemplate, AppKey, query);
+            var response = client.GetAsync(searchQuery).Result.EnsureSuccessStatusCode();
+            return response.Content.ReadAsAsync<SearchResult>();
+        }
+    }
+
+    public static class Semantics3ShoppingSearchClient
+    {
+        public static void GetProductsAsync(string query)
+        {
+            Products products = new Products(ConfigurationManager.AppSettings["Semantics3:APIKey"], ConfigurationManager.AppSettings["Semantics3:APISecret"]);
+            products.products_field("search", query);
+            //products.products_field("cat_id", 4992);
+            //products.products_field("brand", "Toshiba");
+            //products.products_field("weight", "gte", 1000000);
+            //products.products_field("weight", "lte", 1500000);
+            //products.products_field("sitedetails", "name", "newegg.com");
+            //products.products_field("sitedetails", "latestoffers", "currency", "USD");
+            //products.products_field("sitedetails", "latestoffers", "price", "gte", 100);
+            JObject apiResponse = products.get_products();
+        }
+    }
+}
